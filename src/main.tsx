df689b9219c89c56b2e0b19a9f8b60661ba16043
Nikhilkoppad <144939564+Nikhilkoppad@users.noreply.github.com>
2026-09-11 18:44:43 +0530

    feat: route app through product entry gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import ProductEntryGate from './ProductEntryGate'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ProductEntryGate />
   </StrictMode>,
 )
